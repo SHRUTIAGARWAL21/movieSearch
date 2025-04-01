f5a3471ba6c93cf42e6fde356517449237fda491
shrutiagarwal21 <khushiagarwal0204@gmail.com>
2025-04-01 23:52:47 +0530

    new commit

--- src/App-v2.js
+++ src/App-v2.js
@@ -52,8 +52,6 @@ const tempWatchedData = [
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
-const KEY = "f84fc31d";
-
 export default function App() {
   const [query, setQuery] = useState("");
   const [movies, setMovies] = useState([]);
@@ -107,7 +105,7 @@ export default function App() {
           setError("");
 
           const res = await fetch(
-            `https://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            `https://www.omdbapi.com/?apikey=${apikey}&s=${query}`,
             { signal: controller.signal }
           );
 
@@ -364,7 +362,7 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
       async function getMovieDetails() {
         setIsLoading(true);
         const res = await fetch(
-          `https://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+          `https://www.omdbapi.com/?apikey=${apikey}&i=${selectedId}`
         );
         const data = await res.json();
         setMovie(data);
--- src/App.js
+++ src/App.js
@@ -4,10 +4,9 @@ import { useMovies } from "./useMovies";
 import { useLocalStorageState } from "./useLocalStorageState";
 import { useKey } from "./useKey";
 
+const API_KEY = process.env.REACT_APP_KEY;
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
-
-const KEY = "f84fc31d";
 
 export default function App() {
   const [query, setQuery] = useState("");
@@ -275,7 +274,7 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
       async function getMovieDetails() {
         setIsLoading(true);
         const res = await fetch(
-          `https://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+          `https://www.omdbapi.com/?apikey=${API_KEY}&i=${selectedId}`
         );
         const data = await res.json();
         setMovie(data);
